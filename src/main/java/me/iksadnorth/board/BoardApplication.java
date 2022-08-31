ede721de4b0d8509af1a7fb42a40a3eda3fed0b1
iksadNorth <iksadnorth@gmail.com>
2022-08-31 18:21:12 +0900

    #5 - spring boots 프로젝트 생

---
+++ src/main/java/me/iksadnorth/board/BoardApplication.java
@@ -0,0 +1,13 @@
+package me.iksadnorth.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+}
