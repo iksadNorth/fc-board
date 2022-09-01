98767c0dddd679fbda6fb621b1402bd7a30914f7
iksadNorth <iksadnorth@gmail.com>
2022-09-02 00:00:44 +0900

    #15 - Vue.js 대략적인 위치 잡기

---
+++ frontend/src/components/parts/com_sheet.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-sheet class="bg-grey pa-2">test</v-sheet>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+
+    },
+
+    created() {
+        
+    },
+
+    mounted() {
+        
+    },
+
+    watch: {
+
+    },
+
+    computed: {
+
+    },
+
+    props: {
+
+    },
+}
+</script>
+
+<style>
+
+</style>
