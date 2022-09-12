b5d2219ce1cae448365ba3a23e2b7261d10fdb9e
iksadNorth <iksadnorth@gmail.com>
2022-09-12 23:05:09 +0900

    #18 - Repository Layer 수준에서의 Commentory 코드 작성. Test코드와 그를 구현한 코드를 커밋.

--- src/main/java/me/iksadnorth/board/repository/CommentoryRepository.java
+++ src/main/java/me/iksadnorth/board/repository/CommentoryRepository.java
@@ -1,7 +1,10 @@
 package me.iksadnorth.board.repository;
 
 import me.iksadnorth.board.domain.Commentory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentoryRepository extends JpaRepository<Commentory, Long> {
+    Page<Commentory> findByPost_Id(Long postId, Pageable page);
 }
